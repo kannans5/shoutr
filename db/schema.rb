e89ed4dd733b2376ff8e0f185f6b8b467200c595
Saravanan <k-saravanan@live.com>
2021-06-04 08:42:17 +0530

    Fix migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_01_080718) do
+ActiveRecord::Schema.define(version: 2021_06_04_031049) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,8 +19,8 @@ ActiveRecord::Schema.define(version: 2021_06_01_080718) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "content_type"
-    t.integer "content_id"
+    t.string "content_type", null: false
+    t.integer "content_id", null: false
     t.index ["content_id", "content_type"], name: "index_shouts_on_content_id_and_content_type"
     t.index ["user_id"], name: "index_shouts_on_user_id"
   end
